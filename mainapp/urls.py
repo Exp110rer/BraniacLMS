811eb5e111bf660872d01d122da279692aceae3e
Exp110rer <a_sakharov@mail.ru>
2022-11-29 23:16:17 +0300

    Removed: news pagonation

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from mainapp.apps import MainappConfig
-from mainapp.views import MainPageView, NewsPageView, CoursesPageView, ContactsPageView, DocSitePageView, LoginPageView, \
-    NewsPagePaginatedView
-
+from mainapp.views import MainPageView, NewsPageView, CoursesPageView, ContactsPageView, DocSitePageView, LoginPageView
 app_name = MainappConfig.name
 
 urlpatterns = [
     path('', MainPageView.as_view(), name="index"),
     path("news/", NewsPageView.as_view(), name="news"),
-    path("news/<int:page>", NewsPagePaginatedView.as_view(), name="news_paginated"),
     path("courses/", CoursesPageView.as_view(), name="courses"),
     path("contacts/", ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", DocSitePageView.as_view(), name="doc_site"),
