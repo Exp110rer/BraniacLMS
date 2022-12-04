395068c8b428d180b4e553cb7e54e85ee555b212
Exp110rer <a_sakharov@mail.ru>
2022-12-04 22:44:22 +0300

    lesson_4 / disabled: custom contect processor

--- mainapp/context_processors/news.py
+++ mainapp/context_processors/news.py
@@ -1,16 +1,16 @@
-import json
-import os
-from pathlib import Path
-from django.conf import settings
+# import json
+# import os
+# from pathlib import Path
+# from django.conf import settings
 
-WORKIN_DIR = os.path.join(Path(__file__).resolve().parent.parent,'news_base')
-news_file = os.path.join(WORKIN_DIR, 'news.json')
+# WORKIN_DIR = os.path.join(Path(__file__).resolve().parent.parent,'news_base')
+# news_file = os.path.join(WORKIN_DIR, 'news.json')
 
 
-def news_list(request):
-    with open (news_file, 'r') as file_obj:
-        news_list = json.load(file_obj)
-    return {'news_list': news_list}
+# def news_list(request):
+#     with open (news_file, 'r') as file_obj:
+#         news_list = json.load(file_obj)
+#     return {'news_list': news_list}
 
 
 
