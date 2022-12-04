2fca06c92bc31f9bdf824e015f8860312fd5fe59
Exp110rer <73857939+Exp110rer@users.noreply.github.com>
2022-12-04 22:14:57 +0300

    Lesson 4 (#3)
    * Add base template
    
    * Add include template
    
    * Add proper url links
    
    * Add static
    
    * Add cycle for news
    
    * Add news paginator
    
    * Add context processot and news via json
    
    * Add template filter for mailto
    
    * Removed: news pagonation
    
    * Removed: context_processor for news list
    
    * Add: mainapp models
    
    * Add: makemigrations
    
    * Add: fixtures and data
    
    * Add: data migration
    
    * Add: detail view News and Courses

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from mainapp.apps import MainappConfig
-from mainapp.views import MainPageView, NewsPageView, CoursesPageView, ContactsPageView, DocSitePageView, LoginPageView, \
-    NewsPagePaginatedView
-
+from mainapp.views import MainPageView, NewsPageView, CoursesPageView, ContactsPageView, DocSitePageView, LoginPageView, NewsPageDetailView, CoursesPageDetailView
 app_name = MainappConfig.name
 
 urlpatterns = [
     path('', MainPageView.as_view(), name="index"),
     path("news/", NewsPageView.as_view(), name="news"),
-    path("news/<int:page>", NewsPagePaginatedView.as_view(), name="news_paginated"),
+    path("news/<int:pk>", NewsPageDetailView.as_view(), name="news_detail"),
     path("courses/", CoursesPageView.as_view(), name="courses"),
+    path("courses/<int:pk>", CoursesPageDetailView.as_view(), name='courses_detail'),
     path("contacts/", ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", DocSitePageView.as_view(), name="doc_site"),
     path("login/", LoginPageView.as_view(), name="login"),
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,30 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
+from mainapp.models import News, Courses, CourseTeachers, Lesson
 
 # Create your views here.
 
 class MainPageView(TemplateView):
     template_name = 'mainapp/index.html'
+    extra_context = {'title': 'Main page'}
     
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['range'] = range(5)
-    #     return context
-
-class NewsPagePaginatedView(NewsPageView):
-    def get_context_data(self, page, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_num'] = page
+        context['news_qs'] = News.objects.all()[0:7]
+        context['title'] = 'News'
+        return context
+
+
+class NewsPageDetailView(TemplateView):
+    template_name = 'mainapp/news_detail.html'
+
+    def get_context_data(self, pk=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news_object'] = get_object_or_404(News, pk = pk)
+        context['title'] = context['news_object'].title
         return context
 
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['course_objects'] = Courses.objects.all()[0:7]
+        context['title'] = 'Courses'
+        return context
+
+
+class CoursesPageDetailView(TemplateView):
+    template_name = 'mainapp/courses_detail.html'
+
+    def get_context_data(self, pk=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['course_object'] = get_object_or_404(Courses, pk=pk)
+        context['teachers'] = CourseTeachers.objects.filter(course=context['course_object'])
+        context['lessons'] = Lesson.objects.filter(course=context['course_object'])
+        context['title'] = context['course_object'].name
+        return context
 
 class ContactsPageView(TemplateView):
     template_name = "mainapp/contacts.html"
