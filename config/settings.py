b643831a60cbd15296f38f2d506026b13353ae76
Exp110rer <a_sakharov@mail.ru>
2022-12-05 01:39:26 +0300

    lesson_5 / add: default avatar

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static',]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
-MEDUAL_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'authapp.CustomUser'
 
